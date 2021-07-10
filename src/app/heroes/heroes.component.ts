da347378d5a8e1f94328a62054dcd375fe618684
Junyoung Yang <tom9744@gmail.com>
2021-07-10 22:13:17 +0900

    [STEP 4] 서비스 추가하기

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HeroService } from '../hero.service'; 
+import { MessageService } from '../message.service'; 
+
 import { Hero } from '../hero';  // 사용자 지정 타입 
-import { HEROES } from '../mock-heroes'; // 임시 영웅 데이터
 
 @Component({
   selector: 'app-heroes',
@@ -10,17 +12,26 @@ import { HEROES } from '../mock-heroes'; // 임시 영웅 데이터
 })
 export class HeroesComponent implements OnInit {
 
-  heroes: Hero[] = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService, 
+              private messageService: MessageService) { }
 
   // 컴포넌트를 초기화하는 로직은 이 메소드에 작성한다.
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   // <li> 요소에 바인딩 된 이벤트 핸들러 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  // 서비스에서 히어로 목록을 받아오는 메서드
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }

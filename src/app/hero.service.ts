16c9b2ffcecc8fd3b7786aacb305268861d35b07
Junyoung Yang <tom9744@gmail.com>
2021-07-11 11:20:58 +0900

    [STEP 5] 네비게이션 추가하기

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -18,4 +18,12 @@ export class HeroService {
     return heroes;
   }
 
+  getHero(id: number): Observable<Hero> {
+    // 지금은 히어로의 `id` 프로퍼티가 항상 존재한다고 간주합니다.
+    // 에러를 처리하는 방법은 다음 튜토리얼에 대해 알아봅니다.
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
+
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,7 +13,6 @@ import { Hero } from '../hero';  // 사용자 지정 타입
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
   constructor(private heroService: HeroService, 
               private messageService: MessageService) { }
@@ -21,12 +20,6 @@ export class HeroesComponent implements OnInit {
   // 컴포넌트를 초기화하는 로직은 이 메소드에 작성한다.
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  // <li> 요소에 바인딩 된 이벤트 핸들러 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   // 서비스에서 히어로 목록을 받아오는 메서드
